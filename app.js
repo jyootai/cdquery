028a365106081be1feeb3ea0acc53c8735b288e1
jyootai <jyootg@gmail.com>
2015-05-16 18:45:20 +0800

    update routes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+routes(app);
 
 app.listen(3000, function(){
   console.log('please open http://localhost:3000, app is listening at port 3000');
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var login = require('../lib/login');
 
-router.get('/', function(req, res){
-  login(function(data){
-    res.render('index', {data: data.nav});
-  })
-});
+module.exports = function(app) {
+  app.get('/', function(req, res){
+    login(function(data){
+      res.render('index', {data: data.nav});
+    })
+  });
+};
 
-module.exports = router;
